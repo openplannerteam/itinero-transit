b564853f8dd22dd1f09395cda26704da2c98cd59
xivk <ben@itinero.tech>
2018-12-09 16:51:03 +0000

    Finished closest stop search.

--- src/Itinero.Transit/Algorithms/Search/StopSearch.cs
+++ src/Itinero.Transit/Algorithms/Search/StopSearch.cs
@@ -12,7 +12,7 @@ namespace Itinero.Transit.Algorithms.Search
         /// <param name="stopsDb">The stops db.</param>
         /// <param name="box">The box to enumerate in.</param>
         /// <returns>An enumerator with all the stops.</returns>
-        public static IEnumerable<IStop> SearchStopsInBox(this StopsDb stopsDb,
+        public static IEnumerable<IStop> SearchInBox(this StopsDb stopsDb,
             (double minLon, double minLat, double maxLon, double maxLat) box)
         {
             var range = new TileRange(box, stopsDb.StopLocations.Zoom);
@@ -33,6 +33,114 @@ namespace Itinero.Transit.Algorithms.Search
                     yield return rangeStopsEnumerator.Current;
                 }
             }
+        }
+        
+        /// <summary>
+        /// Enumerates all stops in the given bounding box.
+        /// </summary>
+        /// <param name="stopsDb">The stops db.</param>
+        /// <param name="lon">The longitude.</param>
+        /// <param name="lat">The latitude.</param>
+        /// <param name="maxDistanceInMeters">The maximum distance in meters.</param>
+        /// <returns>An enumerator with all the stops.</returns>
+        public static IStop SearchClosest(this StopsDb stopsDb,
+            double lon, double lat, double maxDistanceInMeters = 1000)
+        {
+            var bestDistance = maxDistanceInMeters;
+            IStop bestStop = null;
+            
+            var box = BoxWithSize((lon, lat), bestDistance);
+            var stops = stopsDb.SearchInBox(box);
+            foreach (var stop in stops)
+            {
+                var stopDistance = DistanceEstimateInMeter(lon, lat, stop.Longitude, stop.Latitude);
+                if (!(stopDistance < bestDistance)) continue;
+                bestStop = stop;
+                bestDistance = stopDistance;
+            }
+
+            return bestStop;
+        }
+        
+        const double RadiusOfEarth = 6371000;
+
+        /// <summary>
+        /// Returns an estimate of the distance between the two given coordinates.
+        /// </summary>
+        /// <remarks>Accuracy decreases with distance.</remarks>
+        private static double DistanceEstimateInMeter(double longitude1, double latitude1, double longitude2, double latitude2)
+        {
+            var lat1Rad = (latitude1 / 180d) * System.Math.PI;
+            var lon1Rad = (longitude1 / 180d) * System.Math.PI;
+            var lat2Rad = (latitude2 / 180d) * System.Math.PI;
+            var lon2Rad = (longitude2 / 180d) * System.Math.PI;
+
+            var x = (lon2Rad - lon1Rad) * System.Math.Cos((lat1Rad + lat2Rad) / 2.0);
+            var y = lat2Rad - lat1Rad;
+
+            var m = System.Math.Sqrt(x * x + y * y) * RadiusOfEarth;
+
+            return m;
+        }
+        
+        /// <summary>
+        /// Offsets this coordinate for a given distance in a given direction.
+        /// </summary>
+        private static (double minLon, double minLat, double maxLon, double maxLat) BoxWithSize(this (double lon, double lat) location,
+            double distance)
+        {
+            var ratioInRadians = distance / RadiusOfEarth;
+
+            var oldLatRadians = location.lat.ToRadians();
+            var oldLonRadians = location.lon.ToRadians();
+            var bearing = ((double)45).ToRadians();
+
+            var maxLatRadians = System.Math.Asin(
+                System.Math.Sin(oldLatRadians) *
+                System.Math.Cos(ratioInRadians) +
+                System.Math.Cos(oldLatRadians) *
+                System.Math.Sin(ratioInRadians) *
+                System.Math.Cos(bearing));
+
+            var maxLonRadians = oldLonRadians + System.Math.Atan2(
+                                   System.Math.Sin(bearing) *
+                                   System.Math.Sin(ratioInRadians) *
+                                   System.Math.Cos(oldLatRadians),
+                                   System.Math.Cos(ratioInRadians) -
+                                   System.Math.Sin(oldLatRadians) *
+                                   System.Math.Sin(maxLatRadians));
+            
+            var maxLat = maxLatRadians.ToDegrees();
+            if (maxLat > 180)
+            {
+                maxLat = maxLat - 360;
+            }
+            var minLat = location.lat - (maxLat - location.lat);
+            
+            var maxLon = maxLonRadians.ToDegrees();
+            if (maxLon > 180)
+            {
+                maxLon = maxLon - 360;
+            }
+            var minLon = location.lon - (maxLon - location.lon);
+            
+            return (minLon, minLat, maxLon, maxLat);
+        }
+        
+        /// <summary>
+        /// Converts degrees to radians.
+        /// </summary>
+        private static double ToRadians(this double degrees)
+        {
+            return (degrees / 180d) * System.Math.PI;
+        }
+        
+        /// <summary>
+        /// Converts radians to degrees.
+        /// </summary>
+        private static double ToDegrees(this double radians)
+        {
+            return (radians / System.Math.PI) * 180d;
         }
     }
 }
--- test/Itinero.Transit.Tests/unit/Algorithm/Search/StopSearchTests.cs
+++ test/Itinero.Transit.Tests/unit/Algorithm/Search/StopSearchTests.cs
@@ -18,11 +18,27 @@ namespace Itinero.Transit.Tests.Algorithm.Search
             var id5 = db.Add("http://irail.be/stations/NMBS/008863011", 4.830207824707031, 51.37328062064337);
             var id6 = db.Add("http://irail.be/stations/NMBS/008863012", 5.538825988769531, 51.177621156752494);
 
-            var stops = db.SearchStopsInBox((4.64, 51.17, 5.54, 51.38));
+            var stops = db.SearchInBox((4.64, 51.17, 5.54, 51.38));
             Assert.NotNull(stops);
 
             var stopsList = stops.ToList();
             Assert.Equal(6, stopsList.Count);
+        }
+        
+        [Fact]
+        public void StopsSearch_ShouldFindClosest()
+        {
+            var db = new StopsDb();
+            var id1 = db.Add("http://irail.be/stations/NMBS/008863354", 4.786863327026367, 51.26277419739382);
+            var id2 = db.Add("http://irail.be/stations/NMBS/008863008", 4.649276733398437, 51.345839804352885);
+            var id3 = db.Add("http://irail.be/stations/NMBS/008863009", 4.989852905273437, 51.22365776470275);
+            var id4 = db.Add("http://irail.be/stations/NMBS/008863010", 4.955863952636719, 51.3254629443313);
+            var id5 = db.Add("http://irail.be/stations/NMBS/008863011", 4.830207824707031, 51.37328062064337);
+            var id6 = db.Add("http://irail.be/stations/NMBS/008863012", 5.538825988769531, 51.177621156752494);
+
+            var stop = db.SearchClosest(4.78686332702636, 51.26277419739382);
+            Assert.NotNull(stop);
+            Assert.Equal(id1, stop.Id);
         }
     }
 }
