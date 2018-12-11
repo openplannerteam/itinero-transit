3679f7fef59f1824a03199944dc42039afd196f9
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-12-11 17:58:22 +0100

    Tying together the algorithms

---
+++ src/Itinero.Transit/Algorithms/CSA/ProfileExtensions.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Itinero.Transit.Data;
+using Itinero.Transit.Journeys;
+
+namespace Itinero.Transit.Algorithms.CSA
+{
+    public class ProfileExtensions
+    {
+        /// <summary>
+        /// Calculates the profiles Journeys for the given coordinates.
+        ///
+        /// Starts with an EAS, then gives profiled journeys.
+        ///
+        /// Note that the profile scan might scan in a window far smaller then the last-arrivaltime
+        /// 
+        /// </summary>
+        /// <param name="profile"></param>
+        /// <param name="depLocation"></param>
+        /// <param name="arrivalLocaiton"></param>
+        /// <param name="startTime"></param>
+        /// <param name="lastArrivalTime"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<Journey<T>> CalculateJourneys<T>
+            (this Profile<T> profile, (uint, uint) depLocation, (uint, uint) arrivalLocaiton,
+            ulong startTime, ulong lastArrivalTime)
+        {
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+    }
+}
