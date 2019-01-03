e103cb839cddb9942f9b7224e8171842d231f268
xivk <ben@itinero.tech>
2019-01-03 11:19:36 +0000

    Added parse script.

--- parse-assembly-version.sh
+++ parse-assembly-version.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 regex="\[assembly: AssemblyInformationalVersion\(\"([0-9]+).([0-9]+).([0-9a-z\-]+)"
 while read -r
 do [[ $REPLY =~ $regex ]] && echo "##teamcity[buildNumber '${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}']"
