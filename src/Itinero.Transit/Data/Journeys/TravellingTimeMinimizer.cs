6ef059c62ece586c96738807164fdd339b2d197a
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-03-05 12:03:12 +0100

    Add test for new metric

--- src/Itinero.Transit/Data/Journeys/TravellingTimeMinimizer.cs
+++ src/Itinero.Transit/Data/Journeys/TravellingTimeMinimizer.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 
 namespace Itinero.Transit.Journeys
 {
-    /// <summary>
-    /// This JourneyStatistic will attempt to optimize the journeys in the following way:
-    ///
-    /// 1) The total time walking is minimized
-    /// and iff the same: 
-    /// 2) The total time travelling in a vehicle is minimized
-    /// and iff the same:
-    /// 3) The smallest transfer time is maximized (e.g. if one journey has a transfer of 2' and one of 6', while another has 3' and 3', the second one is chosen.
-    /// and iff the same and an importance list is given
-    /// 4) The biggest stations to transfers are chosen (as bigger station often have better facilities)*
-    ///    > Here, the journeys are compared segment by segment. The difference between every station is taken, the one which performs best overall is taken
-    /// 
-    /// This statistic is meant to be used _after_ PCS in order to weed out multiple journeys which have an equal performance on total travel time and number of transfers.
-    ///
-    /// * Once upon a time, I was testing an early implementation. That EAS implementation gave me a transfer of 1h in Angleur
-    /// (which is a small station), while I could have transfered in Liege as well (a big station with lots of facilities).
-    ///
-    /// The closest food I could find at 19:00 was around one kilometer away.
-    /// 
-    /// </summary>
+    /// <inheritdoc />
+    ///  <summary>
+    ///  (aka: The Pragmatic Statistic)
+    ///  This JourneyStatistic will attempt to optimize the journeys in the following way:
+    ///  1) The total time walking is minimized
+    ///  and iff the same: 
+    ///  2) The total time travelling in a vehicle is minimized
+    ///  and iff the same:
+    ///  3) The smallest transfer time is maximized (e.g. if one journey has a transfer of 2' and one of 6', while another has 3' and 3', the second one is chosen.
+    ///  and iff the same and an importance list is given
+    ///  4) The biggest stations to transfers are chosen (as bigger station often have better facilities)*
+    ///     &gt; Here, the journeys are compared segment by segment. The difference between every station is taken, the one which performs best overall is taken
+    ///  This statistic is meant to be used _after_ PCS in order to weed out multiple journeys which have an equal performance on total travel time and number of transfers.
+    ///  * Once upon a time, I was testing an early implementation. That EAS implementation gave me a transfer of 1h in Angleur
+    ///  (which is a small station), while I could have transfered in Liege as well (a big station with lots of facilities).
+    ///  The closest food I could find at 19:00 was around one kilometer away.
+    ///  </summary>
     public partial class TravellingTimeMinimizer : IJourneyStats<TravellingTimeMinimizer>
     {
         private readonly uint _totalTimeWalking;
---
+++ test/Itinero.Transit.Tests/Data/Journeys/TravelingTimeMinimizerTest.cs
@@ -0,0 +1,40 @@
+using System;
+using Itinero.Transit.Journeys;
+using Xunit;
+
+namespace Itinero.Transit.Tests.Data
+{
+    public class TravelingTimeMinimizerTest
+    {
+        [Fact]
+        public void Ttm0()
+        {
+            var genesis =
+                new Journey<TravellingTimeMinimizer>((0, 0), new DateTime(2019, 03, 05, 10, 00, 00).ToUnixTime(),
+                    TravellingTimeMinimizer.Factory);
+
+            var j0 = genesis.ChainForward(new ConnectionMock(1,
+                new DateTime(2019, 03, 05, 10, 05, 00).ToUnixTime(),
+                new DateTime(2019, 03, 05, 10, 55, 00).ToUnixTime(),
+                2,
+                (0, 0), (0, 1)
+            ));
+
+            var j1 = genesis.ChainForward(new ConnectionMock(1,
+                new DateTime(2019, 03, 05, 10, 05, 00).ToUnixTime(),
+                new DateTime(2019, 03, 05, 10, 50, 00).ToUnixTime(),
+                2,
+                (0, 0), (0, 1)
+            ));
+
+
+            var comp = TravellingTimeMinimizer.Minimize;
+
+            Assert.Equal(1, comp.ADominatesB(j0, j1));
+            Assert.Equal(-1, comp.ADominatesB(j1, j0));
+            
+            Assert.Equal(0, comp.ADominatesB(j0, j0));
+            Assert.Equal(0, comp.ADominatesB(j1, j1));
+        }
+    }
+}
