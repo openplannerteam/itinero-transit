e74c33500e2673e16cac50b7cafedd046597e9e3
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-03-20 15:09:21 +0100

    Start of implementation

---
+++ src/Itinero.Transit.IO.OSM/Data/BusRouteLoader.cs
@@ -0,0 +1,16 @@
+namespace Itinero.Transit.Data
+{
+    /// <summary>
+    /// Loads a PTv2-route relation from OSM, and adds it to a transitDB (for a given timerange)
+    /// </summary>
+    public class BusRouteLoader
+    {
+
+        private uint _relationId;
+
+        public BusRouteLoader(uint relationId)
+        {
+            _relationId = relationId;
+        }
+    }
+}
