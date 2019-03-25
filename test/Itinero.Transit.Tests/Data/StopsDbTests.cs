59dff082d08355ad61561143a00a2ede14253f17
xivk <ben@itinero.tech>
2019-03-25 15:31:55 +0000

    Renamed incorrectly named test.

--- test/Itinero.Transit.Tests/Data/StopsDbTests.cs
+++ test/Itinero.Transit.Tests/Data/StopsDbTests.cs
@@ -110,7 +110,7 @@ namespace Itinero.Transit.Tests.Data
         }
 
         [Fact]
-        public void TiledLocationIndex_WriteToReadFromShouldBeCopy()
+        public void StopsDb_WriteToReadFromShouldBeCopy()
         {
             var db = new StopsDb();
             var id1 = db.Add("http://irail.be/stations/NMBS/008863354", 4.786863327026367, 51.26277419739382,
