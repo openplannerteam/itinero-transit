a31befc51810c4d835bbf815f38e211aab4d8b56
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-09-05 15:38:22 +0200

    Add warning for incorrectly configured profiles

--- src/Itinero.Transit/Data/Profile.cs
+++ src/Itinero.Transit/Data/Profile.cs
@@ -1,5 +1,6 @@
 using Itinero.Transit.Algorithms.Filter;
 using Itinero.Transit.Journey;
+using Itinero.Transit.Logging;
 using Itinero.Transit.OtherMode;
 
 namespace Itinero.Transit.Data
@@ -29,6 +30,14 @@ namespace Itinero.Transit.Data
             JourneyFilter = journeyFilter;
             InternalTransferGenerator = internalTransferGenerator;
             WalksGenerator = walksGenerator;
+            if (InternalTransferGenerator.Range() != 0)
+            {
+                Log.Warning("The profile has an internal transfer generator with range != 0. This is highly suspicious. Perhaps you swapped the InternalTransferGenerator and WalkGenerator-arguments?");
+            }
+            if (WalksGenerator.Range() < 1)
+            {
+                Log.Warning("The profile has an walkGenerator with range 0 (or very small). This is highly suspicious in real-life deployements. (Ignore this if you do not want intermodal transfers before, during or after the journeys)");
+            }
         }
     }
 }
