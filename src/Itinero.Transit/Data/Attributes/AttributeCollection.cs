b75bfe640ecd11fa4867642ed81e94b8193fa98b
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-10-02 16:55:01 +0200

    Removed obsolete code

--- src/Itinero.Transit/Data/Attributes/AttributeCollection.cs
+++ src/Itinero.Transit/Data/Attributes/AttributeCollection.cs
@@ -47,13 +47,7 @@ namespace Itinero.Transit.Data.Attributes
         /// <summary>
         /// Gets the count.
         /// </summary>
-        public int Count
-        {
-            get
-            {
-                return _attributes.Count;
-            }
-        }
+        public int Count => _attributes.Count;
 
         /// <summary>
         /// Clears all data.
@@ -66,13 +60,7 @@ namespace Itinero.Transit.Data.Attributes
         /// <summary>
         /// Gets the readonly flag.
         /// </summary>
-        public bool IsReadonly
-        {
-            get
-            {
-                return false;
-            }
-        }
+        public bool IsReadonly => false;
 
         /// <summary>
         /// Tries to get the value for the given key.
