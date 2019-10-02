8a9a232797212241648c4ae6c88b80e4a902f0d0
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-10-02 15:08:55 +0200

    Add script to quickly start a calculateAll

---
+++ src/Itinero.Transit.Processor/burnCPU.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# One day, someone is gonna ask himself WTF this script does and why it wasn't named appropriately
+# That person is probably gonna be me
+# This script calculates the from/to for all stations in belgium over 12 cores
+
+konsole --hold --tabs-from-file konsoleTabs
