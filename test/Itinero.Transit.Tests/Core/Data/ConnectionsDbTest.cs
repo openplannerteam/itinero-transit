02f0ffef742668ab74451c2abeda4fafc9f59dc5
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-10-14 19:00:24 +0200

    Update tests to refactored version

--- test/Itinero.Transit.Tests/Core/Data/ConnectionsDbTest.cs
+++ test/Itinero.Transit.Tests/Core/Data/ConnectionsDbTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using Itinero.Transit.Data;
 using Itinero.Transit.Data.Core;
+using Itinero.Transit.Data.ReminiscenceConnectionsDb;
 using Reminiscence.Arrays;
 using Xunit;
 
@@ -76,6 +77,33 @@ namespace Itinero.Transit.Tests.Core.Data
                 {
                     throw new Exception($"Index {i} doesn't match: {a[i]}, {b[i]}");
                 }
+            }
+        }
+
+        [Fact]
+        public void ConnectionsDb_Write0TimeConnectionsForward_AssumeRightOrder()
+        {
+            var connDb = new ConnectionsDb(0);
+            var stop0 = new StopId(0, 0, 0);
+            var stop1 = new StopId(0, 0, 1);
+            var stop2 = new StopId(0, 0, 2);
+            var stop3 = new StopId(0, 0, 3);
+
+            var trip0 = new TripId(0, 0);
+
+            connDb.Add(new Connection(new ConnectionId(0, 0), "0", stop0, stop1, 1000, 0, trip0));
+            connDb.Add(new Connection(new ConnectionId(0, 1), "1", stop1, stop2, 1000, 0, trip0));
+            connDb.Add(new Connection(new ConnectionId(0, 2), "2", stop2, stop3, 1000, 1000, trip0));
+
+
+            var enumerator = connDb.GetDepartureEnumerator();
+
+            var c = new Connection();
+            for (uint i = 0; i < 3; i++)
+            {
+                enumerator.MoveNext();
+                enumerator.Current(c);
+                Assert.Equal(i, c.Id.InternalId);
             }
         }
     }
