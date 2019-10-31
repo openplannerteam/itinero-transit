b316075729e5c36ce6a840a4266bec2ad8cc9498
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-10-31 10:25:39 +0100

    Fix typos

--- src/Itinero.Transit.Processor/GenTestCases.sh
+++ src/Itinero.Transit.Processor/GenTestCases.sh
@@ -8,7 +8,7 @@ dotnet run --ctlc https://graph.irail.be/sncb/connections https://graph.irail.be
 
 
 echo "Loading all delijn datasets"
-dotnet run --ctlc "https://openplanner.ilabt.imec.be/delijn/Antwerpen/connections"       "https://openplanner.ilabt.imec.be/delijn/Antwerpen/stops" "$DATE"T00:00 86400 --write-transit-db fixed-test-cases-de-lijn-ant-"$DATE".transitdb
+dotnet run --ctlc "https://openplanner.ilabt.imec.be/delijn/Antwerpen/connections"     "https://openplanner.ilabt.imec.be/delijn/Antwerpen/stops" "$DATE"T00:00 86400 --write-transit-db fixed-test-cases-de-lijn-ant-"$DATE".transitdb
 
 dotnet run --ctlc "https://openplanner.ilabt.imec.be/delijn/Limburg/connections"         "https://openplanner.ilabt.imec.be/delijn/Limburg/stops" "$DATE"T00:00 86400 --write-transit-db fixed-test-cases-de-lijn-lim-"$DATE".transitdb
 
